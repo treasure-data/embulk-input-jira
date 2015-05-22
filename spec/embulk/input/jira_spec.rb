0c408200a1c25636b4782b5bc4e466ed02662737
yoshihara <h.yoshihara@everyleaf.com>
2015-05-22 13:55:56 +0900

    Setup InputPlugin spec skeleton

---
+++ spec/embulk/input/jira_spec.rb
@@ -0,0 +1,14 @@
+require "embulk/command/embulk_run"
+classpath_dir = Embulk.home("classpath")
+jars = Dir.entries(classpath_dir).select {|f| f =~ /\.jar$/ }.sort
+jars.each do |jar|
+  require File.join(classpath_dir, jar)
+end
+require "embulk/java/bootstrap"
+require "embulk"
+require "embulk/input/jira"
+
+require "spec_helper"
+
+describe Embulk::Input::JiraInputPlugin do
+end
