bab0ebd1577bc056fc26b40d42f41192b738b4ed
yoshihara <h.yoshihara@everyleaf.com>
2015-05-25 16:27:15 +0900

    spec: Add spec for JiraInputPlugin#run

--- spec/embulk/input/jira_spec.rb
+++ spec/embulk/input/jira_spec.rb
@@ -14,4 +14,54 @@ describe Embulk::Input::JiraInputPlugin do
       allow(Jira::Api).to receive(:setup)
     end
   end
+
+  describe "#run" do
+    subject do
+      Embulk::Input::JiraInputPlugin.new(task, nil, nil, page_builder).run
+    end
+
+    let(:page_builder) { Object.new } # add mock later
+    let(:task) do
+      {
+        "attributes" => {"project.key" => "string"}
+      }
+    end
+
+    let(:field) do
+      {
+        "fields" =>
+        {
+          "project" => {
+            "id" => "FOO",
+          },
+        }
+      }
+    end
+
+    let(:commit_report) { {} }
+
+    before do
+      # TODO: create stubs without each `it` expected
+      allow(Jira::Api).to receive(:setup).and_return(Jira::Api.new)
+
+      allow_any_instance_of(Jira::Api).to receive(:search_issues).and_return [Jira::Issue.new(field)]
+      allow(page_builder).to receive(:add)
+      allow(page_builder).to receive(:finish)
+    end
+
+    it 'search JIRA issues' do
+      expect_any_instance_of(Jira::Api).to receive(:search_issues)
+      subject
+    end
+
+    it 'page build and finish' do
+      expect(page_builder).to receive(:add)
+      expect(page_builder).to receive(:finish)
+      subject
+    end
+
+    it 'returns commit report' do
+      expect(subject).to eq commit_report
+    end
+  end
 end
