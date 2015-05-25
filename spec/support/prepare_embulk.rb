d37bedd7d87ae4c15310881cc69da8eb6772a6a5
yoshihara <h.yoshihara@everyleaf.com>
2015-05-25 14:21:52 +0900

    Remove needless space

--- spec/support/prepare_embulk.rb
+++ spec/support/prepare_embulk.rb
@@ -2,7 +2,7 @@ module PrepareEmbulk
   require "embulk/command/embulk_run"
 
   classpath_dir = Embulk.home("classpath")
-  jars = Dir.entries(classpath_dir).select {|f| f =~ /\.jar$/ }.sort
+  jars = Dir.entries(classpath_dir).select{|f| f =~ /\.jar$/ }.sort
   jars.each do |jar|
     require File.join(classpath_dir, jar)
   end
