b65d3bf14a9c705ec44cae1bd46d71eaf7361923
yoshihara <h.yoshihara@everyleaf.com>
2015-05-25 10:53:06 +0900

    Move Embulk::Input::JiraInputPlugin.generate_record to Jira::Issue#generate_record

--- lib/embulk/input/jira.rb
+++ lib/embulk/input/jira.rb
@@ -54,7 +54,7 @@ module Embulk
 
         # TODO: we use 0..10 issues to guess config?
         records = jira.search_issues(jql)[0..10].map do |issue|
-          generate_record(issue.fields)
+          issue.generate_record
         end
 
         columns = Guess::SchemaGuess.from_hash_records(records).map do |c|
@@ -73,37 +73,6 @@ module Embulk
         }
 
         return guessed_config
-      end
-
-      def self.generate_record(fields)
-        record = {}
-        fields.each_pair do |key, value|
-          field_key = key.dup
-
-          if value.is_a?(String)
-            field_value = value
-          else
-
-            # TODO: refactor...
-            if value.is_a?(Hash)
-              if value.keys.include?("name")
-                field_key << ".name"
-                field_value = value["name"]
-              elsif value.keys.include?("id")
-                field_key << ".id"
-                field_value = value["id"]
-              else
-                field_value = value.to_json.to_s
-              end
-            else
-              field_value = value.to_json.to_s
-            end
-          end
-
-          record[field_key] = field_value
-        end
-
-        record
       end
 
       def init
--- lib/jira/issue.rb
+++ lib/jira/issue.rb
@@ -19,5 +19,36 @@ module Jira
         fields
       end
     end
+
+    def generate_record
+      record = {}
+      fields.each_pair do |key, value|
+        field_key = key.dup
+
+        if value.is_a?(String)
+          field_value = value
+        else
+
+          # TODO: refactor...
+          if value.is_a?(Hash)
+            if value.keys.include?("name")
+              field_key << ".name"
+              field_value = value["name"]
+            elsif value.keys.include?("id")
+              field_key << ".id"
+              field_value = value["id"]
+            else
+              field_value = value.to_json.to_s
+            end
+          else
+            field_value = value.to_json.to_s
+          end
+        end
+
+        record[field_key] = field_value
+      end
+
+      record
+    end
   end
 end
