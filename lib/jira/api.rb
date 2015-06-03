7229807ed20a9ac017e2aea0989eb62f1cfcc05c
uu59 <k@uu59.org>
2015-06-03 17:13:30 +0900

    separate timeout & retry logic to be a method

--- lib/jira/api.rb
+++ lib/jira/api.rb
@@ -4,7 +4,8 @@ require "timeout"
 
 module Jira
   class Api
-    SEARCH_TIMEOUT = 60
+    SEARCH_TIMEOUT = 5
+    SEARCH_ISSUES_TIMEOUT = 60
     SEARCH_RETRY_TIMES = 10
 
     def self.setup(&block)
@@ -13,27 +14,37 @@ module Jira
     end
 
     def search_issues(jql, options={})
-      search(jql, options).issues.map do |issue|
-        ::Jira::Issue.new(issue)
+      timeout_and_retry(SEARCH_ISSUES_TIMEOUT, SEARCH_RETRY_TIMES) do
+        search(jql, options).issues.map do |issue|
+          ::Jira::Issue.new(issue)
+        end
       end
     end
 
     def search(jql, options={})
-      times = 1
-      begin
-        Timeout.timeout(SEARCH_TIMEOUT) do
-          Jiralicious.search(jql, options)
-        end
-      rescue Timeout::Error
-        times += 1
-        sleep times # retry after some seconds for JIRA API perhaps under the overload
-        raise "JIRA API was too many timeouts" if times > SEARCH_RETRY_TIMES
-        retry
+      timeout_and_retry(SEARCH_TIMEOUT, SEARCH_RETRY_TIMES) do
+        Jiralicious.search(jql, options)
       end
     end
 
     def total_count(jql)
       search(jql, max_results: 1).num_results
+    end
+
+    private
+
+    def timeout_and_retry(wait, retry_times = 10, &block)
+      times = 1
+      begin
+        Timeout.timeout(wait) do
+          yield
+        end
+      rescue Timeout::Error => e
+        times += 1
+        sleep times # retry after some seconds for JIRA API perhaps under the overload
+        raise e if times > retry_times
+        retry
+      end
     end
   end
 end
