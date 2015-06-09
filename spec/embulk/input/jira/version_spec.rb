fccc9a9f3847a2b34cb2bdfedcbce993b8e84992
Kei Takahashi <k.takahashi@everyleaf.com>
2015-06-09 11:54:08 +0900

    Remove unnecessary spec

--- spec/embulk/input/jira/version_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe Embulk::Input::Jira::VERSION do
-  subject { Embulk::Input::Jira::VERSION }
-
-  it "x.y.z format" do
-    expect(subject).to match(/\A[0-9]+\.[0-9]+\.[0-9]+\z/)
-  end
-end
