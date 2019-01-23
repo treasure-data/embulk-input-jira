32ab5029aa34cd3b73d41123c1480a70a4c61cc2
Phu Nguyen <phu@treasure-data.com>
2019-01-23 11:06:41 +0700

    Fixed CI

--- src/main/java/org/embulk/input/jira/Issue.java
+++ src/main/java/org/embulk/input/jira/Issue.java
@@ -13,7 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class Issue
